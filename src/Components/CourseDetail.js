22e7f46335335819ba4a46a5958b4f4c25a501b5
Wes Basinger <wbasinger@villagetechschools.org>
2018-03-13 18:59:10 +0000

    Created links to courseWork details.

--- src/Components/CourseDetail.js
+++ src/Components/CourseDetail.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import api from '../api';
 
 class CourseDetail extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            courseWork : []
+        }
+    }
+    
+    componentDidMount() {
+        api.getAllCourseWork(this.props.match.params.courseId).then((result) => {
+            this.setState({courseWork: result})
+        })
+    }
     
     
     render() {
@@ -8,6 +25,22 @@ class CourseDetail extends React.Component {
         return(
             <div>
                 <h1>Course Detail for {this.props.match.params.courseName}</h1>
+                
+                {
+                    this.state.courseWork.length ?
+                    
+                    <div>
+                        {
+                            
+                            this.state.courseWork.map((cw) => {
+                            
+                                const composedUrl = `/course/${this.props.match.params.courseId}/courseWork/${cw.id}`
+                            
+                                return(<Link to={composedUrl}><p>{cw.title}</p></Link>)
+                            })
+                        }
+                    </div> : <div>Loading assignments....</div>
+                }
             </div>
         )
     }
