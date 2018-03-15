0c8cc4f35c04efcb3a53f64f00818810e7f17c8e
Wes Basinger <wbasinger@villagetechschools.org>
2018-03-15 01:56:37 +0000

    applied styles to header component.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,54 @@
 import React from 'react';
 
 const Header = (props) => {
-    
+   
     return(
-        <div>
-            Google Classroom Data Pipeline
-            {
-                props.loggedIn ?
-                    <div>
-                        Name: {props.name}
-                        <img alt="profile pic" src={props.imageUrl} />
-                        <button onClick={()=>{
-                            props.onLogout();
-                        }}>Log Out</button>
-                    </div> :
-                    <div>
-                        Please log in.
-                    </div>
-            }
-        </div>
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+            <div className="container">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="/">Google Classroom Data Pipeline</a>
+                </div>
+                {
+                    props.loggedIn ?
+                        <div id="navbar" className="collapse navbar-collapse">
+                            <ul className="nav navbar-nav">
+                                <li>Name: {props.name}</li>
+                                <li><img height="40px" alt="profile pic" src={props.imageUrl} /></li>
+                                <li>
+                                    <button onClick={()=>{
+                                        props.onLogout();
+                                    }}>Log Out</button>
+                                </li>
+                            </ul>
+                        </div> :
+                        <div id="navbar" className="collapse navbar-collapse">
+                            Please log in.
+                        </div>
+                }
+            </div>
+        </nav>
     )
 }
 
 export default Header;
+
+    // <nav >
+    //   <div class="container">
+    //     <div >
+    //       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+    //         <span class="sr-only">Toggle navigation</span>
+    //         <span class="icon-bar"></span>
+    //         <span class="icon-bar"></span>
+    //         <span class="icon-bar"></span>
+    //       </button>
+    //       
+    //     </div>
+    //     <div >
+    //       <ul >
+    //         <li class="active"><a href="#">Home</a></li>
+    //         <li><a href="#about">About</a></li>
+    //         <li><a href="#contact">Contact</a></li>
+    //       </ul>
+    //     </div><!--/.nav-collapse -->
+    //   </div>
+    // </nav>
